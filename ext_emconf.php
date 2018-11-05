2e07d60836ee9fbe160dabddaa5d7546790ecbdf
prdt3e <pr@network-media.de>
2018-11-05 01:20:34 +0100

    Update ext_emconf.php
    Update depends 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,8 +12,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '8.7.4',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
-            'rte_ckeditor' => '8.7.0-8.7.99',
+            'typo3' => '8.7.0-9.5.99',
+            'rte_ckeditor' => '8.7.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [
